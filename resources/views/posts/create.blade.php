fab66f1d69cff29970b975e4da3593c259d00d49
Kuredew <kureichi@gmail.com>
2025-08-29 11:09:47 +0700

    feat: Add image field in create reoute

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,14 +13,23 @@
         </div>
     @endif
 
-    <form action="{{route('posts.store') }}" method="POST">
+    <form class="card-body" action="{{route('posts.store') }}" method="POST">
         @csrf
-        <label>Title:</label>
-        <input type="text" name="title" value="{{ old('title') }}">
+        <div class="from group mb-3">
+            <label>image</label>
+            <input class="form-control"  type="file" name="image">
+        </div>
 
-        <label>Content:</label>
-        <textarea name="content">{{ old('content') }}</textarea>
+        <div class="from group mb-3">
+            <label>Title</label>
+            <input class="form-control"  type="text" name="title" value="{{ old('title') }}">
+        </div>
 
-        <button type="submit">Submit</button>
+        <div class="from group mb-3">
+            <label>Content:</label>
+            <textarea class="form-control" name="content">{{ old('content') }}</textarea>
+        </div>
+
+        <button type="submit" class="btn-primary">Submit</button>
     </form>
 @endsection
