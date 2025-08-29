6d9ee84d25c448bd719875dcb2b4b2048396c1b4
Kuredew <kureichi@gmail.com>
2025-08-29 10:40:24 +0700

    refactor: Comment unused route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::resource('posts', PostController::Class);
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
