1116af5fdc0681f335a1a84e1f015acb9e790bb7
kureichi <hibikumonogatari@gmail.com>
2025-09-03 08:12:13 +0700

    feat: Modifying web Routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+//use App\Http\Controllers\PostController;
+use App\Http\Controllers\Auth\LoginRegisterController;
 
-Route::resource('posts', PostController::Class);
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::get('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::get('/Authenticate', 'Authenticate')->name('Authenticate');
+    Route::get('/home', 'home')->name('home');
+    Route::get('/logout', 'logout')->name('logout');
+});
+
+//Route::resource('posts', PostController::Class);
 /*
 Route::get('/', function () {
     return view('welcome');
