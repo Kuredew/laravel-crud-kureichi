b6eea7dded1eb22354a11933767843940d6b13fd
Kuredew <burrohmanhabib35@gmail.com>
2025-09-12 00:47:12 +0700

    feat: Masukin image kedalam show Posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layout')
 
 @section('content')
+    @if ($post->image)
+        <img src="{{ asset('storage/images/'. $post->image) }}" alt="image" style="min-height: 150px; max-height: 150px;">
+    @endif
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->content }}</p>
     <a href="{{ route('posts.index') }}">Back to list</a>
